94fe5e16b94e8625e69bbbf3b4733b7bb18c64e1
jaren.xiu <jarenmarcelino76@gmail.com>
2025-02-13 22:28:19 +0800

    first commit

---
+++ recipebook/ledger/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import recipes, recipe1, recipe2, index
+
+
+urlpatterns = [
+    path("", index, name="index"), 
+    path("recipes/list", recipes, name="recipes"),
+    path("recipe/1", recipe1, name="recipe1"),
+    path("recipe/2", recipe1, name="recipe2")
+]
+
+# This might be needed, depending on your Django version
+app_name = "ledger"
