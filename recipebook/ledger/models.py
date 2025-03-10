d961f7db624d870c39c05da95c3d99c7ad4fb8f9
jaren.xiu <jarenmarcelino76@gmail.com>
2025-03-10 14:48:29 +0800

    Added profile model, updated recipe model, updated recipe detail page

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse 
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -12,8 +12,11 @@ class Ingredient(models.Model):
         return reverse("ledger:ingredient_detail", args=[self.pk])
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=100) 
-
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return self.name
 
@@ -26,3 +29,11 @@ class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
